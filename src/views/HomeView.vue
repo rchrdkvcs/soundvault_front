f6d7b4a81f9d920e0d1ab065fae0b49ebc782d02
rchrdkvcs <b34r.rk@gmail.com>
2025-03-31 19:37:50 +0200

    chore: vue front init with vite

---
+++ src/views/HomeView.vue
@@ -0,0 +1,7 @@
+<script lang="ts" setup></script>
+
+<template>
+  <main class="flex flex-col items-center justify-center min-h-screen bg-gray-950">
+    <h1 class="color-white text-2xl font-medium">Bienvenue sur SoundVault</h1>
+  </main>
+</template>
